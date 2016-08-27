6fd9a561a653907b2c4833232857f417673497b1
Xkynar <xkynar@gmail.com>
2016-08-27 06:23:14 +0100

    Circular Map and Basic Dinosaur Controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    [SerializeField] private Camera mainCam, leftCam, rightCam;
+    [SerializeField] private SpriteRenderer groundRenderer;
+    [SerializeField] private Transform player;
+
+    private Vector3 groundExtents;
+
+	void Start ()
+    {
+        groundExtents = groundRenderer.bounds.extents;
+
+        Vector3 camOffset = groundExtents * 2f;
+        camOffset.y = 0;
+        camOffset.z = 0;
+
+        leftCam.transform.position -= camOffset;
+        rightCam.transform.position += camOffset;
+	}
+	
+	void Update ()
+    {
+        //Loop player
+        if (player.position.x > groundExtents.x)
+        {
+            player.position = new Vector3(-groundExtents.x, player.position.y, player.position.z);
+        }
+
+        if (player.position.x < -groundExtents.x)
+        {
+            player.position = new Vector3(groundExtents.x, player.position.y, player.position.z);
+        }
+
+        //Follow player
+        transform.position = player.position;
+	}
+}
---
+++ Assets/Scripts/DinosaurController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DinosaurController : MonoBehaviour {
+
+    [SerializeField] private Rigidbody2D motor;
+    [SerializeField] private HingeJoint2D joint;
+
+    [SerializeField] private float motorVelocity = 100f;
+    [SerializeField] private float jointVelocity = 100f;
+
+	void Start () 
+    {
+	
+	}
+	
+	void Update ()
+    {
+        UpdateMovement();
+    }
+
+    private void UpdateMovement()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            motor.angularVelocity = -motorVelocity;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            motor.angularVelocity = motorVelocity;
+        }
+        else
+        {
+            motor.angularVelocity = 0f;
+        }
+
+        JointMotor2D jmotor = joint.motor;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            jmotor.motorSpeed = -jointVelocity;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            jmotor.motorSpeed = jointVelocity;
+        }
+        else
+        {
+            jmotor.motorSpeed = 0f;
+        }
+
+        joint.motor = jmotor;
+    }
+}
